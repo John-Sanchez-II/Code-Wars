95043a66a30af2cb1a25a16882985c209a5d2e1f
oneIwant <jsanchez0937@gmail.com>
2022-08-09 23:27:07 -0400

    converting number to string

--- Higher-Order-Functions#6.js
+++ Higher-Order-Functions#6.js
@@ -7,9 +7,30 @@ false otherwise.
 For example, given the following input array:*/
 
 let list = [
-  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
-  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
-  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+  {
+    firstName: "Daniel",
+    lastName: "J.",
+    country: "Aruba",
+    continent: "Americas",
+    age: 42,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Kseniya",
+    lastName: "T.",
+    country: "Belarus",
+    continent: "Europe",
+    age: 22,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Hanna",
+    lastName: "L.",
+    country: "Hungary",
+    continent: "Europe",
+    age: 65,
+    language: "JavaScript",
+  },
 ];
 /*your function should return true.
 
@@ -19,18 +40,21 @@ The strings representing a given language will always be formatted in the same w
 The input array will always be valid and formatted as in the example above.
 
  boolean
-   return statement
-  index language
-  const output = list[i].language
-  if output !== */
-                              
-  function isSameLanguage(list) {
-    for (let i = 0; i < list.length; i++) {
+ return statement
+ index language
+ const output = list[i].language
+ if output !== */
+// .every
+function isSameLanguage(list) {
+  for (let i = 0; i < list.length; i++) {
     if (list[0].language !== list[i].language) {
-    return false;
+      return false;
     }
-    }
-    return true;
   }
-// console.log(isSameLanguage(list))
+  return true;
+}
+//  console.log(isSameLanguage(list))
 
+function isSameLanguage(list) {
+  return list.every()
+}
